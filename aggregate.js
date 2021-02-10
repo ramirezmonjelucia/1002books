2fd988fab9dcbe06d8a03c599a3a4cb429c0e437
Lucia Ramirez Monje <lramirez@iespuntadelverde.es>
2021-02-10 21:32:36 +0100

    first commit

--- aggregate.js
+++ aggregate.js
@@ -1,46 +1,46 @@
 db.books.aggregate([
-        {
-            '$match': {
-                '$expr': {
-                    '$gt': [
-                        {
-                            '$year': '$Fecha'
-                        }, 2018
+    {
+        '$match': {
+            '$expr': {
+                '$gt': [
+                    {
+                        '$year': '$Fecha'
+                    }, 2018
+                ]
+            }
+        }
+    }, {
+        '$group': {
+            '_id': '$Autor',
+            'Total_Libros_Vendidos': {
+                '$sum': '$Vendidos'
+            },
+            'DineroVentas': {
+                '$sum': '$Precio€'
+            }
+        }
+    }, {
+        '$project': {
+            'Autor': '$_id',
+            '_id': 0,
+            'Total_Libros_Vendidos': 1,
+            'Dinero_total_obtenido': {
+                '$sum': {
+                    '$multiply': [
+                        '$Total_Libros_Vendidos', '$DineroVentas'
                     ]
                 }
             }
-        }, {
-            '$group': {
-                '_id': '$Autor', 
-                'Total_Libros_Vendidos': {
-                    '$sum': '$Vendidos'
-                }, 
-                'DineroVentas': {
-                    '$sum': '$Precio€'
-                }
-            }
-        }, {
-            '$project': {
-                'Autor': '$_id', 
-                '_id': 0, 
-                'Total_Libros_Vendidos': 1, 
-                'Dinero_total_obtenido': {
-                    '$sum': {
-                        '$multiply': [
-                            '$Total_Libros_Vendidos', '$DineroVentas'
-                        ]
-                    }
-                }
-            }
-        }, {
-            '$sort': {
-                'Dinero_total_obtenido': -1
-            }
-        }, {
-            '$match': {
-                'Dinero_total_obtenido': {
-                    '$gt': 700000
-                }
+        }
+    }, {
+        '$sort': {
+            'Dinero_total_obtenido': -1
+        }
+    }, {
+        '$match': {
+            'Dinero_total_obtenido': {
+                '$gt': 700000
             }
         }
-    ]).pretty();
+    }
+]).pretty();
